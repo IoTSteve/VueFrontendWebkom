b71a1f329518eb801f27880e8ed2d25fb03d2259
Steve <S_Reich.1@web.de>
2020-05-04 18:18:25 +0200

    First commit
    first commit of vue server test

---
+++ src/views/Plan.vue
@@ -0,0 +1,46 @@
+
+<template>
+
+ 
+
+<div class="Plan">
+  <h1>{{ msg }}</h1>
+    
+  <select name="wochentage" id="wochentage">
+    <option>Montag</option>
+    <option>Dienstag</option>
+    <option>Mittwoch</option>
+    <option>Donnerstag</option>
+    <option>Freitag</option>
+  </select>
+
+   <br>
+        <table>
+          <tr>Kategorie</tr>
+          <tr>Infos</tr>
+          <tr>Kosten</tr>
+          <tr>Tag</tr>
+          <tr>Downvotes</tr>
+          <tr>ID</tr>
+          <tr>Labels</tr>
+          <tr>Name</tr>
+          <tr>Upvotes</tr>
+        </table>
+  
+  
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Plan',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
